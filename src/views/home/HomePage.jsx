e1b06449bf2dfe37fd3d9f7cf4c0862bac1fcafb
Bijoy Prasad Modi <bpmodi88@gmail.com>
2025-07-18 08:49:53 +0530

    HomePage & Footer updated, BannerSlider, Title, CategorySlider, MealSlider added

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -1,9 +1,34 @@
-
+import {
+  BannerSlider,
+  CategorySlider,
+  MealSlider,
+  Title,
+} from "../../components/common";
+import { pattern_one } from "../../utils/images";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <main className="home-page custom-min-h pt-[4px]">
+      <BannerSlider />
+      <section
+        className="categories"
+        style={{
+          background: `url('${pattern_one}') center/cover no-repeat`,
+        }}
+      >
+        <div className="container">
+          <Title subTitle="Choose a Category" mainTitle="Recipe Categories" />
+        </div>
+        <CategorySlider />
+      </section>
+      <section className="meals">
+        <div className="container">
+          <Title subTitle="Get Meal Ready" mainTitle="Recipe Meals" />
+          <MealSlider />
+        </div>
+      </section>
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
